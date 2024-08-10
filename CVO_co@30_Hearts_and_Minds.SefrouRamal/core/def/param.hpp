5145bd616ea4cdc440e33cccfe9bb8466cbd8e12
Mr. Zorn <dennisAzorn@gmail.com>
2024-08-10 19:42:29 +0200

    disable shared slot

--- CVO_co@30_Hearts_and_Minds.SefrouRamal/core/def/param.hpp
+++ CVO_co@30_Hearts_and_Minds.SefrouRamal/core/def/param.hpp
@@ -135,7 +135,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_SLOT_ISSHARE"]);
         values[]={0,1};
         texts[]={$STR_DISABLED, $STR_ENABLED};
-        default = 1;
+        default = 0;
     };
     class btc_p_type_title { // << Faction options >>
         title = $STR_BTC_HAM_PARAM_FAC_TITLE;
