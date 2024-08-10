9019328fe58491f0365f252206d4bf5da97119ec
Mr. Zorn <dennisAzorn@gmail.com>
2024-08-10 19:26:22 +0200

    increase City spawn radius

--- CVO_co@30_Hearts_and_Minds.SefrouRamal/core/def/param.hpp
+++ CVO_co@30_Hearts_and_Minds.SefrouRamal/core/def/param.hpp
@@ -465,7 +465,7 @@ class Params {
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_SPAWNRAD"]);
         values[]={0,1,2,3,4,5,6,7,8};
         texts[]={"0 m","100 m","200 m",$STR_BTC_HAM_PARAM_OTHER_SPAWNRAD_DEF,"400 m","500 m (Takistan)","600 m","700 m","800 m"}; // texts[]={"0 m","100 m","200 m","300 m","400 m","500 m (Takistan)","600 m","700 m","800 m"};
-        default = 3;
+        default = 8;
     };
     class btc_p_trigger { // Disable city activation when a plane or helicopter (>190Km/h) is flying above:
         title = __EVAL(format ["      %1", localize "STR_BTC_HAM_PARAM_OTHER_DISABLESPAWN"]);
